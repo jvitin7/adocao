a64ac8242aec80fbdde714c9053670c8c238c710
João Vitor Vigarani <joao.vigarani7@gmail.com>
2023-04-06 00:12:51 -0300

    Feat: Add ability to edit data on each

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,37 @@
 import { Request, Response } from "express";
 
 export const home = (request: Request, response: Response) => {
-  response.render("pages/page");
+  response.render("pages/page", {
+    banner: {
+      title: "Todos os animais",
+      background: "allanimals.jpg",
+    },
+  });
 };
+
 export const dogs = (request: Request, response: Response) => {
-  response.send("dogs");
+  response.render("pages/page", {
+    banner: {
+      title: "Cachorros",
+      background: "banner_dog.jpg",
+    },
+  });
 };
+
 export const cats = (request: Request, response: Response) => {
-  response.send("cats");
+  response.render("pages/page", {
+    banner: {
+      title: "Gatos",
+      background: "banner_cat.jpg",
+    },
+  });
 };
+
 export const fishes = (request: Request, response: Response) => {
-  response.send("fishes");
+  response.render("pages/page", {
+    banner: {
+      title: "Peixes",
+      background: "banner_fish.jpg",
+    },
+  });
 };
